85675367c7b2f93883d967742ccb7e41b579d9dc
omaressalmani <124935179+omaressalmani@users.noreply.github.com>
2025-11-01 16:32:21 +0000

    Deploying to gh-pages from @ omaressalmani/omaressalmani.github.io@9fd12d49c3e8c20e71bfe660313a0a3f7dea3994 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "omaressalmani/omar-essalmani.github.io",
+    "data-repo": "omaressalmani/omaressalmani.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
