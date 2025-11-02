bc8e3dd4bef48310df451d77ed8716dfa43d0025
omaressalmani <124935179+omaressalmani@users.noreply.github.com>
2025-11-02 00:32:12 +0000

    Deploying to gh-pages from @ omaressalmani/omaressalmani.github.io@3f12fa536af7b9330edeab3e2a23b150cf8d7c4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,16 +9,16 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-projects",
+          title: "Projects",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume-cv",
+          title: "Resume / CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
