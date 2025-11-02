05046b5c10d59293ee80647992503bc0f7995956
omaressalmani <124935179+omaressalmani@users.noreply.github.com>
2025-11-02 13:17:36 +0000

    Deploying to gh-pages from @ omaressalmani/omaressalmani.github.io@242ec0ac0fc5260518e55a83ac692c12b008b127 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-resume-cv",
           title: "Resume / CV",
           description: "",
